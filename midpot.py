295e09406046cba6974e5f56c16f15251ba8e959
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2016-03-28 20:10:13 -0700

    addded documentation

--- midpot.py
+++ midpot.py
@@ -1,66 +1,115 @@
+##
+## The code below solves Steven Halim's CP3
+## potentionmeter problem (CP3 p12086)
+##
+## The chosen datastructure is a Fenwick tree
+## or a binary index tree (binex tree)
+##
+## A Fenwick tree allows for efficient 
+## updates of the partial sum of an array
+## and efficient lookups of the partial sum
+## of an array
+##
+## update O(log N)
+## get_sum O(log N)
+## build tree O(N log N)
+##
+
+## returns the next index 
 def get_next( i ):
 	return i + (-i & i)
 
+## returns the parent index
 def get_parent( i ):
 	return i - (- i &i)
 
+## updates the partial sum
+## for all affected nodes
 def update ( i , value , binex ) :
 	
 		while i < len( binex ) :
-				print("Updating index %d by %d" % (i, value))
 				binex[i] += value
 				i = get_next(i) 
 
+## traverses from node j until
+## node i is reached, summating
+## partial sums along the way
 def get_sum ( i , j , binex ) :
 		
 		sum = 0
 
 		while  j >= i :
-				print("Adding %d at index %d to sum %d to yield %d" % (binex[j], j, sum, sum + binex[j])) 
 				sum += binex[j]
 				j = get_parent(j)
 
 		return sum
 
+## COMMANDS FOR USER: 
+
+## defines a command for measuring
+## partial sums between two potmeters
+#
+## USAGE: M $1 $2
+#
+## $1 - the index of the potmeter
+## to be measured from the left
+## $2 - the index of the potmeter
+## to be measured from the right
+
 def measure ( param, binex ) : 
 		print( get_sum ( int(param[0]) , int(param[1]) , binex ) )  
 
+
+## defines a command for setting 
+## the value of a potmeter
+#
+## USAGE: S $1 $2
+#
+## $1 - the index of the potmeter
+## to be set
+#
+## $2 - the value to set potmeter
+## to
+
 def set ( param, binex ) :
+		
 		i = int( param[0] )
 		value = int( param[1] )
 
 		update ( i , value - binex[i] , binex )
 
 def main():
+
+	## command map
 	commands = {'S' :  set , 'M' : measure }
 
 	run = True
 	case = 0
 
+	## 0 to exit run loop
 	while run: 
 		
 			case += 1
 
-			print("Case %d:" % case ) 
-
 			N = int( input() )
 
 			run = N > 0 
+		
+			if(run == False):
+				break
+
+			print("Case %d:" % case ) 
 
 			binex = [0]*(N+1)
 
-			print(binex)
-
 			for i in range(1,N+1):
-					print("Set potmeter %d to: " % i)
 					update ( i , int(input()) , binex)
-					print(binex)
 
 			test = True
 
+			## END to exit test loop
 			while test:
 			
-					print("Enter command S or M")
 					command = input().split(' ') 
 					parameters = command[1 : len(command)] 
 					command = command[0]
@@ -70,6 +119,4 @@ def main():
 					else:
 						commands[ command ]( parameters ,  binex )
 					
-					print(binex)
-
 main()
