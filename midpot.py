9852a9f3970d57aebed0e16d834c29757a533e28
Patrick Michaelsen <prmichaelsen@users.noreply.github.com>
2016-03-29 16:23:51 -0700

    Update midpot.py
    added more documentation

--- midpot.py
+++ midpot.py
@@ -1,6 +1,5 @@
 from sys import exit
 
-
 ## The code below solves Steven Halim's CP3
 ## potentionmeter problem (CP3 p12086)
 ##
@@ -10,7 +9,7 @@ from sys import exit
 ## A Fenwick tree allows for efficient 
 ## updates of the partial sum of an array
 ## and efficient lookups of the partial sum
-## of an array
+## of an array (prefix sum)
 ##
 ## update O(log N)
 ## get_sum O(log N)
@@ -59,9 +58,20 @@ def get_sum ( j , binex ) :
 ## to be measured from the right
 
 def measure ( param, binex , values) : 
-		## $1 - 1 allows for measurement from left
+		
+		## measure the prefix sum for the right potmeter
 		sum_high = get_sum( int( param[1] ) , binex )
+		
+		## measure the prefix sum for the
+		## left potmeter
+		## $1 - 1 allows for measurement 
+		## from the "left terminal"
 		sum_low = get_sum( int( param[0] ) - 1 , binex )
+		
+		## print the values
+		## this is bad practice,
+		## as this method is called "measure"
+		## not "measure_and_print
 		print( sum_high - sum_low )  
 		
 
@@ -79,13 +89,22 @@ def measure ( param, binex , values) :
 
 def set ( param, binex , values) :
 		
+		## node to be updated
 		i = int( param[0] )
+		
+		## new value is the value
+		## to set, save old value
+		## and update table of values 
+		## with new value
 		new_value = int( param[1] )
 		old_value = values[i]
 		values[i] = new_value
 
+		## update the tree with the 
+		## change in values
 		update ( i , new_value - old_value , binex )
 
+## testing utility
 def print_binex ( param, binex , values) : 
 		print(binex)
 
@@ -106,24 +125,45 @@ def main():
 
 	## 0 to exit run loop
 	while run: 
-		
+	
 			case += 1
 
+			## input the size of next 
+			## fenwick tree
 			N = int( input() )
 
+			## do not run on N = 0
 			run = N > 0 
 		
+			## lazy loop exit
+			## this is not good practice
 			if(run == False):
 				break
 
 			print("Case %d:" % case ) 
 
+			## binex is the fenwick tree
+			## N+1 because binex[0] is
+			## a dummy element. This is
+			## similar to many implementations
+			## of a binary heap
 			binex = [0]*(N+1)
+			
+			## values is the table of values
+			## it is useful to keep this
+			## information as it improves
+			## the implementation of the "set"
+			## command
 			values = [0]*(N+1)
 
+			## read in the table of values
 			for i in range(1,N+1):
 					value = int( input() )
+					
+					## update the fenwick tree
 					update ( i , value , binex)
+					
+					## update table of values
 					values[i] = value
 
 			test = True
@@ -131,10 +171,12 @@ def main():
 			## END to exit test loop
 			while test:
 			
+					## input next command
 					command = input().split(' ') 
 					parameters = command[1 : len(command)] 
 					command = command[0]
 
+					## run command if not END
 					if command == "END" :
 							test = False
 					else:
